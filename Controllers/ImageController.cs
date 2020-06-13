c4cac4c330c0b9fbd0f992b4e786949b2a2fe7b6
Erfan Alizadeh Noohi <erfan.noohi@gmail.com>
2020-06-14 00:48:46 +0430

    Add project files.

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace ImageApi.Controllers
+{
+    [Route("Image")]
+    public class ImageController : Controller
+    {
+        // POST: api/Image
+        [HttpPost]
+        public async Task Post(IFormFile file)
+        {
+            var uploads = Path.Combine("D:\\projects\\Net Core\\Image-Serve\\ImageApi\\StaticRoot", "uploads");
+            if (file.Length > 0)
+            {
+                using var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create);
+                await file.CopyToAsync(fileStream);
+            }
+        }
+    }
+}
